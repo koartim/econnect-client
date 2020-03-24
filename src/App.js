53b5128d536ff8c1f80620e0fdd4b70427eb4756
Tim Koar <koartimothy@gmail.com>
2020-03-24 13:35:05 -0400

    connected with backend api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Employees from './Employees';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+
+  state = {
+    employees: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8080/api/v1/employees')
+    .then(rsp => rsp.json())
+    .then(data => {
+      this.setState({
+        employees: data.employeeDTOS
+      })
+    })
+  }
+  render() {
+    return(
+      <div>
+        {this.state.employees.map(emp => {
+          return <h1>{emp.firstName}</h1>
+        })}
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
